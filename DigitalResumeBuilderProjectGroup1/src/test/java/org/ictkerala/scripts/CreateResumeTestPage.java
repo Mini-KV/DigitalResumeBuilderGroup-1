9c86f0c0459b543af24f46ef1a3acf14a3e12214
Mini-KV <137192486+Mini-KV@users.noreply.github.com>
2023-07-29 12:47:05 +0530

    Update CreateResumeTestPage.java

--- DigitalResumeBuilderProjectGroup1/src/test/java/org/ictkerala/scripts/CreateResumeTestPage.java
+++ DigitalResumeBuilderProjectGroup1/src/test/java/org/ictkerala/scripts/CreateResumeTestPage.java
@@ -11,9 +11,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-/*
- * @author Mini
- */
+
 public class CreateResumeTestPage extends TestBase{
 	
 	CreateResumePage createres;
