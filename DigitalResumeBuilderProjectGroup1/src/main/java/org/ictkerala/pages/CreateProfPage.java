eba608605abef19d4b85fbbc8f2e62e5a1e3bfdb
Mini-KV <137192486+Mini-KV@users.noreply.github.com>
2023-07-29 12:45:31 +0530

    Update CreateProfPage.java
    Updated comments

--- DigitalResumeBuilderProjectGroup1/src/main/java/org/ictkerala/pages/CreateProfPage.java
+++ DigitalResumeBuilderProjectGroup1/src/main/java/org/ictkerala/pages/CreateProfPage.java
@@ -12,9 +12,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-/*
- * @author Mini
- */
 public class CreateProfPage {
 	WebDriver driver;
 	WebDriverWait wait;
